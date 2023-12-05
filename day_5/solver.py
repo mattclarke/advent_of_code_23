7d44d4678673d12a1e17c28bb8d20ab2edb45c66
Matt Clarke <matt.clarke@ess.eu>
2023-12-06 08:19:25 +0900

    5: tidied up part 1

--- day_5/solver.py
+++ day_5/solver.py
@@ -8,7 +8,6 @@ with open(FILE) as f:
     PUZZLE_INPUT = f.read()
 
 lines = [line.strip() for line in PUZZLE_INPUT.split("\n")]
-print(lines)
 
 _, s = lines[0].split(": ")
 seeds = [int(x.strip()) for x in s.split(" ")]
@@ -16,93 +15,36 @@ seeds = [int(x.strip()) for x in s.split(" ")]
 lines.pop(0)
 lines.pop(0)
 
-seed_to_soil = []
-soil_to_fert = []
-fert_to_water = []
-water_to_light = []
-light_to_temp = []
-temp_to_humid = []
-humid_to_loc = []
+rules = [[] for _ in range(7)]
 
 index = 0
-while index < len(lines):
-    line = lines[index]
-    if line.startswith("seed-"):
+for line in lines:
+    if not line:
         index += 1
-        while lines[index]:
-            l = lines[index].split(" ")
-            ans = [int(x.strip()) for x in l]
-            seed_to_soil.append(ans)
-            index += 1
-    if line.startswith("soil-"):
-        index += 1
-        while lines[index]:
-            l = lines[index].split(" ")
-            ans = [int(x.strip()) for x in l]
-            soil_to_fert.append(ans)
-            index += 1
-    if line.startswith("fertilizer-"):
-        print("fert")
-        index += 1
-        while lines[index]:
-            l = lines[index].split(" ")
-            ans = [int(x.strip()) for x in l]
-            fert_to_water.append(ans)
-            index += 1
-    if line.startswith("water-"):
-        index += 1
-        while lines[index]:
-            l = lines[index].split(" ")
-            ans = [int(x.strip()) for x in l]
-            water_to_light.append(ans)
-            index += 1
-    if line.startswith("light-"):
-        index += 1
-        while lines[index]:
-            l = lines[index].split(" ")
-            ans = [int(x.strip()) for x in l]
-            light_to_temp.append(ans)
-            index += 1
-    if line.startswith("temperature-"):
-        index += 1
-        while lines[index]:
-            l = lines[index].split(" ")
-            ans = [int(x.strip()) for x in l]
-            temp_to_humid.append(ans)
-            index += 1
-    if line.startswith("humidity-"):
-        index += 1
-        while lines[index]:
-            l = lines[index].split(" ")
-            ans = [int(x.strip()) for x in l]
-            humid_to_loc.append(ans)
-            index += 1
-    index += 1
+    elif line[0].isdigit():
+        ans = [int(x.strip()) for x in line.split(" ")]
+        # TODO: store the diff and the end points
+        diff = ans[0] - ans[1]
+        h = ans[1] + ans[2] - 1
+        rules[index].append([ans[1], h, diff])
 
-result = 10000000000
-
-current = list(seeds)
 
 def solve(current, convert):
     result = []
     for s in current:
         ans = s
         for c in convert:
-            dest, src, r = c
-            if src <= s < src + r:
-                diff = s - src
-                ans = dest + diff
+            start, end, diff = c
+            if start <= s <= end:
+                ans = s + diff
                 break
         result.append(ans)
     return result
 
-current = solve(current, seed_to_soil) 
-current = solve(current, soil_to_fert) 
-current = solve(current, fert_to_water) 
-current = solve(current, water_to_light) 
-current = solve(current, light_to_temp) 
-current = solve(current, temp_to_humid) 
-current = solve(current, humid_to_loc) 
+
+current = list(seeds)
+for rule in rules:
+    current = solve(current, rule)
 
 
 # Part 1 = 227653707
@@ -110,7 +52,7 @@ print(f"answer = {min(current)}")
 
 current = []
 for i in range(0, len(seeds), 2):
-    current.append([seeds[i], seeds[i] + seeds[i+1]-1])
+    current.append([seeds[i], seeds[i] + seeds[i + 1] - 1])
 
 
 def solve(current, convert):
@@ -134,7 +76,7 @@ def solve(current, convert):
                 l_diff = l - src
                 h_diff = h - src
                 result.append([dest + l_diff, dest + h_diff])
-            elif src <= l < src+r:
+            elif src <= l < src + r:
                 # only l is in range
                 print("l")
                 changed = True
@@ -156,25 +98,26 @@ def solve(current, convert):
             result.append([l, h])
     return result
 
+
 print("======")
 print(current)
-current = solve(current, seed_to_soil) 
+current = solve(current, seed_to_soil)
 print(current)
 print("======")
-current = solve(current, soil_to_fert) 
+current = solve(current, soil_to_fert)
 print(current)
 print("======")
-current = solve(current, fert_to_water) 
+current = solve(current, fert_to_water)
 print(current)
 print("======")
-current = solve(current, water_to_light) 
+current = solve(current, water_to_light)
 print(current)
 print("======")
-current = solve(current, light_to_temp) 
+current = solve(current, light_to_temp)
 print(current)
 print("======")
-# current = solve(current, temp_to_humid) 
-# current = solve(current, humid_to_loc) 
+# current = solve(current, temp_to_humid)
+# current = solve(current, humid_to_loc)
 
 # Part 2 = 78775051
 print(f"answer = {min(current)}")
