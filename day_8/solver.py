f167348594fcd11cb15e0b17d0608b11246cf4b4
Matt Clarke <matt.clarke@ess.eu>
2023-12-08 14:40:45 +0900

    9: part 1 done

--- day_8/solver.py
+++ day_8/solver.py
@@ -10,10 +10,29 @@ with open(FILE) as f:
 lines = [line.strip() for line in PUZZLE_INPUT.split("\n") if line]
 print(lines)
 
-result = 0
+route = lines.pop(0)
+
+M = {}
 
 for l in lines:
-    pass
+    l = l.replace(" = ", " ").replace("(", "").replace(")", "").replace(",", "")
+    pos, left, right = l.split(" ")
+    M[pos] = (left, right)
+
+result = 0
+loc = "AAA"
+index = 0
+
+while loc != "ZZZ":
+    choice = route[index % len(route)]
+    if choice == "L":
+        loc = M[loc][0]
+    else:
+        loc = M[loc][1]
+    index += 1
+    result += 1
+
+
 
 # Part 1 = 
 print(f"answer = {result}")
