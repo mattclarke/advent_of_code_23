1c6f43a43a61279aa301eff6422b84c0788d9346
Matt Clarke <matt.clarke@ess.eu>
2023-12-13 14:34:16 +0900

    13: part 1

--- day_13/solver.py
+++ day_13/solver.py
@@ -7,18 +7,58 @@ FILE = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
 with open(FILE) as f:
     PUZZLE_INPUT = f.read()
 
-lines = [line.strip() for line in PUZZLE_INPUT.split("\n") if line]
-print(lines)
+lines = [line.strip() for line in PUZZLE_INPUT.split("\n")]
+
+patterns = []
+current = []
+for l in lines:
+    if l:
+        current.append(l)
+    else:
+        patterns.append(current)
+        current = []
+
+
+def find_reflection(rows):
+    prev = None
+    for ri, row in enumerate(rows):
+        if prev and prev == row:
+            # Potential reflection
+            ia = ri - 1
+            ib = ri
+            is_reflection = True
+            while ia >= 0 and ib < len(rows):
+                if rows[ia] != rows[ib]:
+                    is_reflection = False
+                    break
+                ia -= 1
+                ib += 1
+            if is_reflection:
+                return ri
+        prev = row
+    return 0
+
 
 result = 0
 
-for l in lines:
-    pass
+for p in patterns:
+    # horizontal
+    horiz = find_reflection(p)
+    result += 100 * horiz
 
-# Part 1 = 
+    # vertical
+    # rotate
+    cols = [[] for _ in p[0]]
+    for row in p:
+        for c, v in enumerate(row):
+            cols[c].append(v)
+    vert = find_reflection(cols)
+    result += vert
+
+# Part 1 = 34993
 print(f"answer = {result}")
 
 result = 0
 
-# Part 2 = 
+# Part 2 =
 print(f"answer = {result}")
