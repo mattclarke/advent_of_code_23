5cbf1af415b1bc9f9f3c7cabb6354f95f3030051
Matt Clarke <matt.clarke@ess.eu>
2023-12-15 14:10:33 +0900

    15: part 1

--- day_15/solver.py
+++ day_15/solver.py
@@ -7,15 +7,26 @@ FILE = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
 with open(FILE) as f:
     PUZZLE_INPUT = f.read()
 
-lines = [line.strip() for line in PUZZLE_INPUT.split("\n") if line]
-print(lines)
+lines = [line.strip() for line in PUZZLE_INPUT.split("\n") if line][0]
+
+codes = lines.split(",")
 
 result = 0
 
-for l in lines:
-    pass
+for code in codes:
+    total = 0
+    for ch in code:
+        v = ord(ch)
+        total += v
+        total *= 17
+        total = total % 256
+    result += total
 
-# Part 1 = 
+        
+
+
+
+# Part 1 = 513158
 print(f"answer = {result}")
 
 result = 0
