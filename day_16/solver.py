e5f33a1bb9c8b1eac2abb5d2c8ee7fa9590ad730
Matt Clarke <matt.clarke@ess.eu>
2023-12-16 17:05:03 +0900

    16: part 1

--- day_16/solver.py
+++ day_16/solver.py
@@ -8,15 +8,88 @@ with open(FILE) as f:
     PUZZLE_INPUT = f.read()
 
 lines = [line.strip() for line in PUZZLE_INPUT.split("\n") if line]
-print(lines)
 
-result = 0
+WIDTH = len(lines[0])
+HEIGHT = len(lines)
 
-for l in lines:
-    pass
+def solve(start, initial_dir):
+    seen = set()
+    Q = [(start, initial_dir)]
+    while Q:
+        (r, c), d = Q.pop(0)
+        while r < HEIGHT and c < WIDTH and r >= 0 and c >= 0:
+            if (r,c,d) in seen:
+                break
+            seen.add((r,c,d))
+            if lines[r][c] == ".":
+                if d == "R":
+                    c += 1
+                elif d == "L":
+                    c -= 1
+                elif d == "D":
+                    r += 1
+                elif d == "U":
+                    r -= 1
+                else:
+                    assert False
+            elif lines[r][c] == "/":
+                if d == "R":
+                    r -= 1
+                    d = "U"
+                elif d == "L":
+                    r += 1
+                    d = "D"
+                elif d == "D":
+                    c -= 1
+                    d = "L"
+                elif d == "U":
+                    c += 1
+                    d = "R"
+                else:
+                    assert False
+            elif lines[r][c] == "\\":
+                if d == "R":
+                    r += 1
+                    d = "D"
+                elif d == "L":
+                    r -= 1
+                    d = "U"
+                elif d == "D":
+                    c += 1
+                    d = "R"
+                elif d == "U":
+                    c -= 1
+                    d = "L"
+                else:
+                    assert False
+            elif lines[r][c] == "|" and d in ["L", "R"]:
+                Q.append(((r+1,c), "D"))
+                r -= 1
+                d = "U"
+            elif lines[r][c] == "-" and d in ["U", "D"]:
+                Q.append(((r, c+1), "R"))
+                c -= 1
+                d = "L"
+            elif lines[r][c] == "|" and d in ["U", "D"]:
+                if d == "U":
+                    r -= 1
+                else:
+                    r += 1
+            elif lines[r][c] == "-" and d in ["L", "R"]:
+                if d == "L":
+                    c -= 1
+                else:
+                    c += 1
+            else:
+                print(lines[r][c], r, c, d)
+                assert False
+    squares = set()
+    for (r,c,_) in seen:
+        squares.add((r,c))
+    return len(squares)
 
-# Part 1 = 
-print(f"answer = {result}")
+# Part 1 = 7632
+print(f"answer = {solve((0,0), 'R')}")
 
 result = 0
 
